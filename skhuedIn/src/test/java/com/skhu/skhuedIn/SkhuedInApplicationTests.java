ecce6ac146d62c129c5d6e41a7e3c83cb4924c78
her0807 <her0807@naver.com>
2021-03-11 14:02:51 +0900

    feat : 프로젝트 생성

---
+++ skhuedIn/src/test/java/com/skhu/skhuedIn/SkhuedInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhu.skhuedIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhuedInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
