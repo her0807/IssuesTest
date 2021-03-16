9728cbee4f766f52f97c8bffdf132dc7faa6c1e3
her0807 <her0807@naver.com>
2021-03-16 09:17:38 +0900

    feat : 레포지토리 추가

--- skhuedIn/src/main/java/com/skhu/skhuedIn/repository/ItemRepository.java
+++ skhuedIn/src/main/java/com/skhu/skhuedIn/repository/ItemRepository.java
@@ -1,2 +1,39 @@
-package com.skhu.skhuedIn.repository;public class ItemRepository {
+package com.skhu.skhuedIn.repository;
+
+
+import com.skhu.skhuedIn.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId()==null){
+            em.persist(item);
+        }else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i",Item.class).getResultList();
+    }
+
+
+
+
+
 }
--- skhuedIn/src/main/java/com/skhu/skhuedIn/repository/MemberRepository.java
+++ skhuedIn/src/main/java/com/skhu/skhuedIn/repository/MemberRepository.java
@@ -1,2 +1,42 @@
-package com.skhu.skhuedIn.repository;public class MemberRepository {
+package com.skhu.skhuedIn.repository;
+
+
+import com.skhu.skhuedIn.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+    public void save(Member member) {
+        em.persist(member);
+
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+    // 단 건 조회
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    // 전체 조회
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name",
+                Member.class)
+                .setParameter("name", name).getResultList();
+    }
 }
