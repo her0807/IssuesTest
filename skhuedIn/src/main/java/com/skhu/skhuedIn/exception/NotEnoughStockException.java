b85d84c8ef1d2eb1ca22690350a39638e2dd88d5
her0807 <her0807@naver.com>
2021-03-16 09:17:16 +0900

    feat : 예외 처리를 위한 패키지 추가

--- skhuedIn/src/main/java/com/skhu/skhuedIn/exception/NotEnoughStockException.java
+++ skhuedIn/src/main/java/com/skhu/skhuedIn/exception/NotEnoughStockException.java
@@ -1,6 +1,23 @@
 package com.skhu.skhuedIn.exception;
 
-public class NotEnoughStockException extends Throwable {
-    public NotEnoughStockException(String need_more_stock) {
+public class NotEnoughStockException extends RuntimeException{
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
